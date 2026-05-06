99b6f58ddca2827840f405308e2767a632d8be31
Om Gupta <guptaomg23@gmail.com>
2026-05-06 15:58:19 +0530

    Update

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://devconnect-1-ukwn.onrender.com";
 export default BASE_URL;
 export const github = "https://github.com/omgupta23";
 export const linkdin = "https://www.linkedin.com/in/om-gupta-338075294/";
